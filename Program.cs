f941c5be0c53148f428774111699a0f329690454
Bryce Palmer <everettraven@gmail.com>
2018-11-27 21:48:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using System.IO.Ports;
+
+using System.Threading;
+
+
+
+/*
+
+    C# console application written by Bryce Palmer to send
+    data to an arduino and the arduino does an action based
+    off of the data recieved. Arduino sends back a character to 
+    signify the data has been recieved.
+
+ */
+
+
+
+namespace BluetoothForTourniquet
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            try
+
+            {
+
+                string[] Ports;
+
+                string input;
+
+                Console.WriteLine("Welcome to the Bluetooth COM Port for the Secret Project");
+
+                Console.WriteLine("------------------------------------------------------------------");
+
+                SerialPort serial = new SerialPort();
+
+                Ports = SerialPort.GetPortNames();
+
+
+
+                Console.WriteLine("Available Ports");
+
+                Console.WriteLine("------------------------------------------------------------------");
+
+
+
+                for (int i = 0; i < Ports.Length; i++)
+
+                {
+
+                    Console.WriteLine(Ports[i]);
+
+                }
+
+                Console.WriteLine("Type which port you want to connect to");
+
+                input = Console.ReadLine();
+
+                Console.WriteLine();
+
+
+
+                serial.PortName = input;
+
+                serial.BaudRate = 9600;
+
+                serial.Parity = Parity.None;
+
+                serial.DataBits = 8;
+
+                serial.StopBits = StopBits.One;
+
+                serial.Open();
+
+                while (true)
+                {
+                    Console.WriteLine("Type the command you would like to run. I (capital i) = Turn on, O = Turn off, E = exit, to change PWM and interval value do as follows -------------> PWMValue:Interval");
+                    string SendData = Console.ReadLine();
+
+                    if(SendData.Contains(":"))
+                    {
+                        serial.Write(SendData);
+                        break;
+                    }
+                    else if(SendData == "I")
+                    {
+                        serial.Write(SendData);
+                        break;
+                    }
+                    else if(SendData == "O")
+                    {
+                        serial.Write(SendData);
+                        break;
+                    }
+                    else if (SendData == "E")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write("That input is invalid");
+                    }
+
+                }
+
+
+               serial.DataReceived += new SerialDataReceivedEventHandler(Serial_DataReceived);
+
+
+                Console.WriteLine("Press any key to quit");
+                Console.ReadKey();
+
+                serial.Close();
+
+
+
+            }
+
+            catch (Exception e)
+
+            {
+
+                Console.WriteLine(e.Message);
+
+                Console.ReadKey();
+
+            }
+
+
+
+        }
+
+
+
+        private static void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+
+        {
+
+            SerialPort sp = (SerialPort)sender;
+
+            string Output = sp.ReadExisting();
+
+            Console.WriteLine(Output);
+
+        }
+
+    }
+
+}
---
+++ TourniquetSketch.ino
@@ -0,0 +1,179 @@
+/*
+ * This is the arduino sketch that runs the tourniquet circuit.
+ * Created By : Bryce Palmer
+ * 10/31/2018
+ */
+ 
+
+//Gets the built-in LED on the arduino
+const int LedPin = LED_BUILTIN;
+
+//int value of the LOW state for an output
+int LedState = LOW;
+
+//setting up an unsigned long to take the value of the millis() function later
+unsigned long PreviousMillis = 0;
+
+//setting a value for an interval
+long TournStartInterval = 5000;
+
+//char value used to get the on/off/change values command from the serial monitor
+char Response = ' ';
+
+//char array to change the values of the resting PWM value and the start to resting interval
+char ValueChanger[];
+
+
+int TournPin = 3;
+
+int TourniquetOn = 0;
+
+//PWM Lower power number
+int LowPower = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  pinMode(LedPin, OUTPUT);
+
+  pinMode(TournPin, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+  while(Serial.available() > 0)
+  {
+    //Get the character inputted through the serial monitor
+    Response = Serial.read();
+
+    //Reprint the character for validation
+    Serial.print(Response);
+
+    if(Response == 'O')
+    {
+      //Check to see if the tourniquet is already on
+      if(TourniquetOn == 1)
+      {
+        
+      }
+      else
+      {
+        //Run the function to turn on the tourniquet
+        PowerOnTourniquet();
+      }
+      
+    }
+    else if(Response == 'I')
+    {
+      //Check to see if the tourniquet is on
+      if(TourniquetOn == 1)
+      {
+        //Run the function to turn off the tourniquet
+        PowerOffTourniquet();        
+      }
+      else
+      {
+        
+      }
+
+    }
+    else if(Response == 'C')
+    {
+      ChangeValues();
+    }
+
+
+  }
+  
+
+}
+
+
+//Function to turn on the tourniquet
+void PowerOnTourniquet()
+{
+  Serial.write("Tourniquet On");
+  
+  digitalWrite(LedPin, HIGH);
+
+  //Set the tourniquet value to "ON"
+  TourniquetOn = 1;
+  
+  //send the required PWM signal to run the circuit
+  analogWrite(TournPin, 255);
+
+  //Set the value of current millis
+  unsigned long CurrentMillis = 0;
+  
+  //delay through millis()
+  //set previous to current millis
+  PreviousMillis = millis();
+  
+  do
+  {
+    //Set current millis and constantly update it
+     CurrentMillis = millis();
+
+    //Check to see if the current - the previous is greater or equal to the interval
+     if(CurrentMillis - PreviousMillis >= TournStartInterval)
+     {
+        break;
+     }
+     //check to see if the command has told the tourniquet to be off
+     else if(Serial.read() == 'I')
+     {
+        //Run the function to turn off the tourniquet
+        PowerOffTourniquet();
+        return 0;
+     }
+     
+  }while(CurrentMillis - PreviousMillis < TournStartInterval);
+
+  //Set the PWM to a lower pulse value to draw less power but keep the tourniquet on
+  analogWrite(TournPin, LowPower);
+
+  
+}
+
+void PowerOffTourniquet()
+{
+  Serial.write("Serial Off");
+  
+  digitalWrite(LedPin, LOW);
+
+  //Set tourniquet 
+  TourniquetOn = 0;
+
+  //Set the PWM pulse value to nothing so the toruniquet turns off
+  analogWrite(TournPin, 0);
+
+  
+}
+
+
+void ChangeValues()
+{
+  char *token;
+  
+  while(Serial.available())
+  {
+    ValueChanger = Serial.readString();
+  }
+
+  token = strtok(ValueChanger, ":");
+
+  LowPower = atoi(token);
+
+  token = strtok(NULL, ":");
+
+  TournStartInterval = atoi(token);
+
+  Serial.write("Data Recieved");
+
+  
+}
